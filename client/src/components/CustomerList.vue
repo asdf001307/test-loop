e0f03baadfe8a02f7e0fbf0f06596fcac1cae7f6
asdf1307 <asdf001307@gmail.com>
2021-09-03 16:17:11 +0900

    0903

--- client/src/components/CustomerList.vue
+++ client/src/components/CustomerList.vue
@@ -1,43 +1,98 @@
 <template>
   <div>
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="id" label="Date" width="180">
+    <el-table :data="customer" style="width: 100%" @cell-click="handle">
+      <!-- <el-table :data="customer" style="width: 100%" @cell-click="handle" >//클릭이벤트 -->
+        <!-- <el-table :data="customer" style="width: 100%"  @cell-click="handleRowClick" >//클릭이벤트 -->
+      <!-- <el-table-column v-for="(item, i) in customer" :key="item" prop="id" label="Date" width="180">
+        {{ customer[i].id }}
+      </el-table-column> -->
+      <el-table-column prop="id" label="Id" width="180">
       </el-table-column>
-      <el-table-column prop="name" label="Name" width="180">
+      <el-table-column prop="customer_code" label="Code" width="180">
+      </el-table-column>
+      <el-table-column prop="customer_name" label="Name">
+      </el-table-column>
+      <el-table-column prop="representative" label="Representative">
       </el-table-column>
       <el-table-column prop="manager" label="Manager">
       </el-table-column>
-      <el-table-column prop="address" label="Address">
+     <el-table-column>
+      <el-button>수정하기</el-button> 
       </el-table-column>
-      <el-table-column prop="address" label="Address">
-        <el-button>수정하기</el-button>
-      </el-table-column>
+      <!-- <template slot-scope="scope" v-if="scope.row"> -->
+      <!-- <el-button>{{ scope.row.id }}수정하기</el-button> -->
+      <!-- </template> -->
+      <!-- <el-table-column v-for="(item, i) in customer" :key="item" prop="id">
+      <el-button @click="Idget">{{customer[i]}}</el-button>
+      </el-table-column> -->
     </el-table>
+      <div>
+    <!-- <ul>
+     <li v-for="(item, i) in customer" :key="item">
+      <h1>{{ customer[i].id }} {{ customer[i].customer_code }} {{ customer[i].customer_name }}</h1>
+    </li>
+    </ul> -->
+        
+  </div>
   </div>
 </template>
 
+
 <script>
-export default {
-  data() {
-    return {
-      tableData: [{
-        date: '2016-05-03',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-02',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-04',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }, {
-        date: '2016-05-01',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      }]
-    }
-  }
-}
+import router from "../router/index.js"
+import axios from 'axios';
+ export default {
+
+    // name: 'SignupForm',
+    data() {
+      return {
+        // customer : {
+        //   code : "",
+        //   name : "",
+        //   representative :"",
+        //   corporate_registration_number :"",
+        //   address :"",
+        //   manager : "",
+        //   create_date : "",
+        //   last_update :"",
+        //   is_delete :""
+
+        // },
+        customer:'',
+        page:1,
+        pageSize :5,
+
+      };
+    },
+    mounted() {
+              axios
+          .get('api/customers').
+        then(response => {
+          this.customer = response.data;
+          //this.customer = JSON.parse(JSON.stringify(response.data));
+          //console.log(response.data)
+          //console.log(response.data[0]['customer_code']);
+          //console.log(response.data);
+          //console.log(response.data[0].address)
+          console.log(this.customer.length);
+          //console.log(this.customer[2]['address']);
+        })
+        .catch(function(error){
+          console.log(error);
+        })
+    },
+  
+    methods: {
+      handle(row){
+        //router.push(customer_update)
+        router.push({ name: 'customer_update', params: { id :row.id, code :row.customer_code, name : row.customer_name }})
+
+        
+      },
+      update(){
+
+      },
+
+    },
+  };
 </script>
--- client/src/components/CustomerUpdate.vue
+++ client/src/components/CustomerUpdate.vue
@@ -1,81 +1,158 @@
 <template>
-<!--  <div id="tabla">-->
-<!--    <div class="container  h-100">-->
-<!--      <div class="row h-100 justify-content-center align-items-center">-->
-<!--        <table class="table table-striped table-bordered table-hover text-center">-->
-<!--          <thead class="thead-dark">-->
-<!--          <tr>-->
-<!--            <th>Name</th>-->
-<!--            <th>Username</th>-->
-<!--            <th>Email</th>-->
-<!--            <th>Phone</th>-->
-<!--          </tr>-->
-<!--          </thead>-->
-<!--          <tbody>-->
-<!--          <tr v-for="customer in customers" :key="customer.id">-->
-<!--            <td>{{ customer.id }}</td>-->
-<!--            <td>{{customer.ci}}</td>-->
-<!--            <td>{{customer.costomer_name}}</td>-->
-<!--            <td>{{customer.costomer_code}}</td>-->
-<!--          </tr>-->
-<!--          </tbody>-->
-<!--        </table>-->
-<!--&lt;!&ndash;        {{ customer }}&ndash;&gt;-->
-<!--&lt;!&ndash;        {{ customer[0] }}<br>&ndash;&gt;-->
-<!--&lt;!&ndash;        {{ customer[1] }}<br>&ndash;&gt;-->
-<!--        {{ customer }}<br>-->
-<!--&lt;!&ndash;        {{ customer[0].id}}<br>&ndash;&gt;-->
-<!--&lt;!&ndash;        {{customer[0]['id']}}&ndash;&gt;-->
-<!--        <button @click="Check()">dd</button>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-  <div>
-    {{ customer[0] }}
-            <button @click="Check()">dd</button>
+  <div class="container">
+    <el-form ref="form" :v-model="customer" label-width="120px">
+      <!-- @submit.prevent="submitForm -->
+      <el-form-item :inline="true" label="고객사이름">
+        <el-input v-model="customer.name"></el-input>
+      </el-form-item>
+      <el-form-item prop="code" label="대표자">
+        <el-input label="대표자" v-model="customer.representative"></el-input>
+      </el-form-item>
+      <el-form-item label="사업자등록번호">
+        <el-input prop="name" v-model="customer.corporate_registration_number"></el-input>
+      </el-form-item>
+      <el-form-item label="주소">
+        <el-input v-model="customer.address"></el-input>
+      </el-form-item>
+      <el-form-item label="담당자">
+        <el-input v-model="customer.manager"></el-input>
+      </el-form-item>
+      <el-form-item label="고객사코드">
+        <el-input v-model="customer.code"></el-input>
+      </el-form-item>
+      <el-button label="고객사코드" @click="CreateCusID(4)">고객사코드 생성</el-button>
+      <el-button @click="CheckcusID">중복체크</el-button>
+      <el-form-item>
+        <el-button type="primary" @click="onUpdate">수정하기</el-button>
+        <el-button>취소하기</el-button>
+      </el-form-item>
+    </el-form>
+<!--    <el-button @click="CreateCusID(4)">고객사코드 생성 :</el-button> {{str}}-->
+<!--    <el-button>중복체크 필요</el-button>-->
   </div>
 </template>
 
 <script>
-import axios from 'axios';
- export default {
+    import axios from 'axios';
+    //import router from '../router';
+    export default {
+      name :"update_customer",
+        data() {
+            return {
+                dt : new Date(),
+                str : "",
+                customer : {
+                    code : "",
+                    name : this.$route.params.id,
+                    representative :this.$route.params.id,
+                    corporate_registration_number :"",
+                    address :"",
+                    manager : "",
+                    create_date : "",
+                    last_update :"",
+                    is_delete :""
 
-    // name: 'SignupForm',
-    data() {
-      return {
-        customer : {
-          code : "",
-          name : "",
-          representative :"",
-          corporate_registration_number :"",
-          address :"",
-          manager : "",
-          create_date : "",
-          last_update :"",
-          is_delete :""
+                },
 
+            };
         },
-
-      };
-    },
-    methods: {
-      Check() {
-        axios
-          .get('api/customers').
+          created() {
+            console.log(this.$route.params.id);
+            console.log(this.$route.params.code);
+            console.log(this.$route.params.name);
+  },
+            mounted() {
+            let filter = {
+              where: {
+                id : 2
+              }
+            };
+              axios
+          .get('api/customers?filter=' + JSON.stringify(filter)).
         then(response => {
           this.customer = response.data;
+          //this.customer = JSON.parse(JSON.stringify(response.data));
           //console.log(response.data)
           //console.log(response.data[0]['customer_code']);
-          console.log(response.data);
+          //console.log(response.data);
           //console.log(response.data[0].address)
-
           console.log(this.customer);
+          //console.log(this.customer[2]['address']);
         })
         .catch(function(error){
           console.log(error);
         })
-      },
-
     },
-  };
+          //     Login() {
+          //   let filter = {
+          //     where: {
+          //       name: this.user.name
+          //     }
+          //   };
+          //   axios.get('api/tests?filter=' + JSON.stringify(filter)).then(response => {
+          //     //(this.test = response.data)
+          //     console.log(response.data[0]['passwd']);
+          //     //console.log(response.data)
+          //     if (this.user.password === response.data[0]['passwd']){
+          //       console.log('success');
+          //       return router.push("/mainpage");
+          //     }
+          //   })
+          // },
+
+        methods: {
+            onUpdate() {
+              axios.put('api/customers',{
+                id : 2,//list에서 방아오기
+                customer_code: this.str,
+                customer_name: this.customer.name,
+                representative: this.customer.representative,
+                corporate_registration_number: this.customer.corporate_registration_number,
+                address: this.customer.address,
+                manager: this.customer.manager,
+                create_date: this.dt.toISOString(),
+                last_update: "2021-09-01T01:59:53.548Z",
+              }).then((res)=>{
+                console.log(res)
+              })
+               console.log('submit');
+              //console.log(Date.now());
+              // var dt = new Date();
+              // console.log(dt.toISOString());
+            },
+            CreateCusID(count) {
+                let str = '';
+
+                for (let i = 0; i < count; i++) {
+                    str += Math.floor(Math.random() * 36).toString(36);
+                }
+                str = str.toUpperCase();
+                //console.log(str);
+                this.str = str;
+                this.customer.code = str;
+                return str;
+            },
+          CheckcusID() {
+            let filter = {
+              where: {
+                customer_code: this.str,
+              }
+            };
+            axios.get('api/customers?filter=' + JSON.stringify(filter)).then(response => {
+              //(this.test = response.data)
+              console.log(response.data)
+              //console.log(response.data[0]['customer_code']);
+              //console.log(response.data)
+              if (response.data.length !== 0){
+                console.log('고객사코드 생성')
+                alert('동일한 고객사 코드 존재');
+                this.CreateCusID(4);
+              }
+            })
+          }
+
+        },
+    };
 </script>
+
+<style scoped></style>
